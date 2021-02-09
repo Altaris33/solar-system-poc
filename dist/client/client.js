fdcc03cbf11a88e30940bd79d000884d5c8bd18e
altaris33 <captain@captain.localmachine>
2021-02-09 01:49:12 +0100

    feat(): added Sun sphere with texture and rotation

--- dist/client/client.js
+++ dist/client/client.js
@@ -1,24 +1,41 @@
 import * as THREE from '/build/three.module.js';
 import { OrbitControls } from '/jsm/controls/OrbitControls';
+//---------------------
 // scene & axes Helper
+//---------------------
 const scene = new THREE.Scene();
-var axesHelper = new THREE.AxesHelper(6);
+var axesHelper = new THREE.AxesHelper(12);
 scene.add(axesHelper);
+scene.add(new THREE.GridHelper(55, 55));
+const centerLight = new THREE.PointLight(0xFFDDFF, 1, 5);
+centerLight.position.set(0, 0, 0);
+scene.add(centerLight);
+//---------------
 // camera 
+//---------------
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(22, 46, 76);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener('change', render);
+//-------------
+// loaders
+//-------------
+const mercuryLoader = new THREE.TextureLoader();
+const mercuryTexture = mercuryLoader.load("assets/textures/sun.jpg");
 // sun 
-const sunGeometry = new THREE.SphereGeometry();
-//console.dir(sphereGeometry)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-const sun = new THREE.Mesh(sunGeometry, material);
-sun.position.y = 4;
+const sunGeometry = new THREE.SphereGeometry(6, 24, 24);
+//console.dir(sunGeometry)
+const sunMaterial = new THREE.MeshBasicMaterial({ map: mercuryTexture });
+const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+sun.position.x = 0;
+sun.position.y = 0;
+sun.position.z = 0;
+sun.scale.setScalar(1.2);
 scene.add(sun);
-camera.position.z = 2;
+// camera.position.z = 10
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -26,18 +43,22 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
 }
+// -------------------------
+// ANIMATION FUNCTION
+// -------------------------
 var animate = function () {
     requestAnimationFrame(animate);
     //cube.rotation.x += 0.01;
     //cube.rotation.y += 0.01;
-    sun.rotation.y += 0.01;
+    sun.rotation.y += 0.002;
     render();
-    document.getElementById("debug1").innerText = "Matrix\n" + sun.matrix.elements.toString().replace(/,/g, "\n");
+    // (document.getElementById("debug1") as HTMLDivElement).innerText = "Matrix\n" + sun.matrix.elements.toString().replace(/,/g, "\n",)
 };
+// -------------------------
+// RENDER FUNCTION
+// -------------------------
 function render() {
-    //stats.begin()
     renderer.render(scene, camera);
-    //stats.end()
 }
 //render() 
 animate();
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,13 +1,24 @@
 import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
 
+//---------------------
 // scene & axes Helper
+//---------------------
 const scene: THREE.Scene = new THREE.Scene()
-var axesHelper = new THREE.AxesHelper(6)
+var axesHelper = new THREE.AxesHelper(12)
 scene.add(axesHelper)
+scene.add(new THREE.GridHelper(55,55))
 
+const centerLight: THREE.PointLight = new THREE.PointLight(0xFFDDFF,1,5);
+centerLight.position.set(0,0,0)
+scene.add(centerLight)
+
+//---------------
 // camera 
+//---------------
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(22,46,76)
+
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -16,17 +27,26 @@ document.body.appendChild(renderer.domElement)
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.addEventListener('change', render)
 
+//-------------
+// loaders
+//-------------
+const mercuryLoader: THREE.TextureLoader = new THREE.TextureLoader()
+const mercuryTexture = mercuryLoader.load("assets/textures/sun.jpg")
+
 // sun 
-const sunGeometry: THREE.SphereGeometry = new THREE.SphereGeometry()
-//console.dir(sphereGeometry)
+const sunGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(6, 24, 24)
+//console.dir(sunGeometry)
 
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+const sunMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ map: mercuryTexture })
 
-const sun: THREE.Mesh = new THREE.Mesh(sunGeometry, material)
-sun.position.y = 4
+const sun: THREE.Mesh = new THREE.Mesh(sunGeometry, sunMaterial)
+sun.position.x = 0
+sun.position.y = 0
+sun.position.z = 0
+sun.scale.setScalar(1.2)
 scene.add(sun)
 
-camera.position.z = 2
+// camera.position.z = 10
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -37,22 +57,32 @@ function onWindowResize() {
 }
 
 
+// -------------------------
+// ANIMATION FUNCTION
+// -------------------------
 var animate = function () {
     requestAnimationFrame(animate)
 
     //cube.rotation.x += 0.01;
     //cube.rotation.y += 0.01;
-    //sun.rotation.y += 0.01
+    sun.rotation.y += 0.002
+    
+
 
     render();
-    (document.getElementById("debug1") as HTMLDivElement).innerText = "Matrix\n" + sun.matrix.elements.toString().replace(/,/g, "\n",)
+    // (document.getElementById("debug1") as HTMLDivElement).innerText = "Matrix\n" + sun.matrix.elements.toString().replace(/,/g, "\n",)
 
 };
 
+
+// -------------------------
+// RENDER FUNCTION
+// -------------------------
+
 function render(){
-    //stats.begin()
+    
     renderer.render(scene, camera)
-    //stats.end()
+    
 }
 
 //render() 
